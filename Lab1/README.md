088d2a34900c010831e44a8a2a30170e7eca13b8
gherman7755 <58359494+gherman7755@users.noreply.github.com>
2020-11-20 11:30:25 +0200

    Add files via upload

---
+++ Lab1/README.md
@@ -0,0 +1,4 @@
+# POO
+# Here was zip files of labs 2-3 
+# But they were disappered
+# May be I sent them on else.fcim
---
+++ Lab1/Univesity.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Univesity {
+    public String name;
+    public int found_year;
+    public ArrayList<Student> students;
+    public Univesity(){
+        students = new ArrayList<Student>();
+    }
+}
