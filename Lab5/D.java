d9760999cc53b0c6fbccf0e396f47692c580fc95
gherman7755 <58359494+gherman7755@users.noreply.github.com>
2020-11-21 13:09:41 +0200

    Add files via upload

---
+++ Lab5/D.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class D extends C{
+    public D(String d){
+        super(d);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
